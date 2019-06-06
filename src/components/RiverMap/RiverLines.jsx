14087646e6deed4200a708078a8942cd26e1840e
kiranbandi <bvenkatkiran@gmail.com>
2019-06-06 07:23:44 -0600

    temp update to flow panel - need to refactor later on

--- src/components/RiverMap/RiverLines.jsx
+++ src/components/RiverMap/RiverLines.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { line } from 'd3';
 import riverCurve from '../../utils/riverCurve';
 
 export default (props) => {
 
     const { lines, xScale, yScale, lineWidth, lineWidthTickRatio } = props;
+
+    _.map(lines, (line) => {
+
+        const { shiftCoords = [0, 0] } = line;
+
+        line.newNodes = _.map(line.nodes, (innerD) => {
+            return { x: xScale(innerD.coords[0] + shiftCoords[0]), y: yScale(innerD.coords[1] + shiftCoords[1]) };
+        })
+
+    });
+
+    const d3Line = line().x((d) => d.x).y((d) => d.y);
+
+
     return (
         <g className='lines-container'>
             {_.map(lines, (d, index) => {
                 return <path
                     key={'river-line-' + index}
                     id={d.name}
-                    d={riverCurve(d, xScale, yScale, lineWidth, lineWidthTickRatio)}
+                    d={riverCurve(d,xScale,yScale,lineWidth,lineWidthTickRatio)}
                     stroke={d.color || '#92cce3'}
                     strokeWidth={lineWidth}
                     className={'river ' + (d.reverse ? 'reverse-flow' : 'forward-flow')}>
