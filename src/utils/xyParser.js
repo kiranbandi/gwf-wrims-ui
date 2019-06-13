2316962faac17931d5ca0f073c21d1181fcb5abb
Ishan Saxena (iss469) <ishan.saxena@usask.ca>
2019-06-13 00:20:55 -0600

    added a few more cases to the parser

--- src/utils/xyParser.js
+++ src/utils/xyParser.js
@@ -3,11 +3,19 @@ import axios from 'axios';
 
 export default function(xyString) {
 
+    var states = {
+        INITIAL:"INITIAL", 
+        READING_NODE:"READING_NODE", 
+        READING_NODE_POS:"READING_NODE_POS", 
+        READING_LINK:"READING_LINK", 
+    }
+
     var stateObj = {
-        states: ["INITIAL_STATE", "READING_NODE"],
-        currentState: "INITIAL_STATE",
-        changeState: function(newState) { this.currentState = newState; }
+        currentState: states.INITIAL,
+        changeState: function(state) { this.currentState = state; }
     };
+
+    
 
 
     var schematicData = { lines: [], artifacts: [], labels: [], markers: [], title: {} }
@@ -16,15 +24,114 @@ export default function(xyString) {
 
         var word = "";
 
+        let nodeObj = {
+            name:"J_39_J_19",
+          color:"#5c6b84",
+          type:"river",
+          reverse:false,
+          shiftCoords:[  
+             0,
+             0
+          ],
+          nodes:[  
+             {  
+                coords:[  
+                   0.558155,
+                   0.06506
+                ]
+             },
+             {  
+                coords:[  
+                   0.477273,
+                   0.063855
+                ]
+             }
+          ]
+        }
+        
+        let linkObj = {
+
+        }
+
         for (const c of response.data) {
             if (!["\r", "\n"].includes(c)) {
                 word = word.concat(c);
-            } 
-            else {
-                if (word !== "")
+            } else {
+                if (word !== "") 
                 {
-                    console.log(word);
-                    word = "";
+                    switch(stateObj.currentState) {
+                        
+                        case states.INITIAL: {
+                            if (word === "node") {
+                                stateObj.changeState(states.READING_NODE);
+                                break;
+                            }
+                            if (word === "link") {
+                                stateObj.changeState(states.READING_LINK);
+                                break;
+                            }
+                            break;
+                        }
+
+                        case states.READING_NODE: {
+                            
+                            let splitWord = word.split(" ");
+                            
+                            switch(splitWord[0]) {
+
+                                case "name": {
+
+                                }
+                                case "num": {
+                                    
+                                }
+                                case "ntype": {
+                                    
+                                }
+                                case "pos": {
+                                    
+                                }
+                            }
+                        }
+
+                        case states.READING_NODE_POS: {
+                            
+                            let splitWord = word.split(" ");
+                            
+                            switch(splitWord[0]) {
+
+                                case "0": {
+
+                                }
+                                case "1": {
+                                    
+                                }
+                            }
+                        }
+
+                        case states.READING_LINK: {
+                            
+                            let splitWord = word.split(" ");
+                            
+                            switch(splitWord[0]) {
+
+                                case "lname": {
+
+                                }
+                                case "lnum": {
+                                    
+                                }
+                                case "fromnum": {
+                                    
+                                }
+                                case "tonum": {
+                                    
+                                }
+                            }
+                        }
+                      }
+
+                      word = "";
                 }
             }
         }
