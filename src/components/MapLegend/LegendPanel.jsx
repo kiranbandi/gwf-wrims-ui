13e5484025f0552eff2591b97d42a328040d9d1d
Ricardo Rheeder <ricardorheeder@gmail.com>
2019-06-27 16:15:49 -0600

    Cleaned up code in egendPanel to make it easier to add new markers

--- src/components/MapLegend/LegendPanel.jsx
+++ src/components/MapLegend/LegendPanel.jsx
@@ -4,118 +4,116 @@ import _ from 'lodash';
 import { line, scaleLinear } from 'd3';
 import LegendIcon from './LegendIcon'
 
-
 export default class LegendPanel extends Component {
     constructor(props) {
         super(props);
     }
 
+
     render() {
+        let svgWidth = 600, // Change this if svg width is changed
+
+            // ICON VARIABLES
+            numberOfIcons = 5, // When adding a new icon, increment numberOfIcons and make sure the increments_Icons is multiplied by the array index
+            increments_Icons = svgWidth / numberOfIcons, startingPoint_Icons = increments_Icons / 2,
+
+            // PATH VARIABLES
+            numberOfPaths = 4, // When adding a new path, increment numberOfPaths and make sure the increments_Paths is multiplied by the array index
+            increments_Paths = svgWidth / numberOfPaths, startingPoint_Paths = (increments_Paths / 2) - 30
+            ;
+
         return (
 
             <div id='map-legend' className='legend-root-container '>
 
                 <div>
                     <div className='filter-div'>
-                        <svg width="600" height="60">
+                        <svg width={svgWidth} height="60">
                             <LegendIcon
                                 title={"IRRIGATION"}
-                                textX={"40"}
-                                circleTransform={"translate(60, 5) scale(0.08)"}
-                                imageTransform={"translate(60, 7) scale(0.048)"}
+                                textX={(startingPoint_Icons + (increments_Icons * 0) - 20)}
+                                circleTransform={"translate(" + (startingPoint_Icons + (increments_Icons * 0)) + ", 5) scale(0.08)"}
+                                imageTransform={"translate(" + (startingPoint_Icons + (increments_Icons * 0)) + ", 7) scale(0.048)"}
                                 type={"agri"}
                             />
 
                             <LegendIcon
                                 title={"NON-IRRIGATION"}
-                                textX={"145"}
-                                circleTransform={"translate(180, 5) scale(0.08)"}
-                                imageTransform={"translate(180, 5) scale(0.055)"}
+                                textX={(startingPoint_Icons + (increments_Icons * 1) - 35)}
+                                circleTransform={"translate(" + (startingPoint_Icons + (increments_Icons * 1)) + ", 5) scale(0.08)"}
+                                imageTransform={"translate(" + (startingPoint_Icons + (increments_Icons * 1)) + ", 5) scale(0.055)"}
                                 type={"demand"}
                             />
 
                             <LegendIcon
                                 title={"INFLOW"}
-                                textX={"288"}
-                                circleTransform={"translate(300, 5) scale(0.08)"}
-                                imageTransform={"translate(300, 5) scale(0.08)"}
+                                textX={(startingPoint_Icons + (increments_Icons * 2) - 12)}
+                                circleTransform={"translate(" + (startingPoint_Icons + (increments_Icons * 2)) + ", 5) scale(0.08)"}
+                                imageTransform={"translate(" + (startingPoint_Icons + (increments_Icons * 2)) + ", 5) scale(0.08)"}
                                 type={"inflow"}
                             />
 
                             <LegendIcon
                                 title={"RESERVOIR"}
-                                textX={"400"}
-                                circleTransform={"translate(420, 5) scale(0.08)"}
-                                imageTransform={"translate(424, 5) scale(0.05)"}
+                                textX={(startingPoint_Icons + (increments_Icons * 3) - 20)}
+                                circleTransform={"translate(" + (startingPoint_Icons + (increments_Icons * 3)) + ", 5) scale(0.08)"}
+                                imageTransform={"translate(" + (startingPoint_Icons + (increments_Icons * 3) + 4) + ", 5) scale(0.05)"}
                                 type={"reservoir"}
                             />
 
                             <LegendIcon
                                 title={"SINK"}
-                                textX={"536"}
-                                circleTransform={"translate(540, 5) scale(0.08)"}
-                                imageTransform={"translate(424, 5) scale(0.05)"}
+                                textX={(startingPoint_Icons + (increments_Icons * 4) - 4)}
+                                circleTransform={"translate(" + (startingPoint_Icons + (increments_Icons * 4)) + ", 5) scale(0.08)"}
                                 type={"sink"}
                             />
-
-                           
-                            {/* <g className='river-marker'
-                                transform="translate(540, 5) scale(0.08)">
-                                <circle
-                                    cx='150' cy='150' r='200'
-                                    className={'type-sink'}>
-                                </circle>
-                                <text x="65" y="250" fontSize="250px" className="sink-text" >S</text>
-                            </g>
-                            <text x="536" y="52.5" className="legend-label">SINK</text> */}
 
                         </svg>
                     </div>
 
                     <div className='filter-div'>
-                        <svg className='flow-data-chart' width="600" height="60">
+                        <svg className='flow-data-chart' width={svgWidth} height="60">
                             <g className='lines-container'>
                                 <path className='flow-spark-line'
-                                    transform={"translate(45, 5)"}
+                                    transform={"translate(" + (startingPoint_Paths + (increments_Paths * 0)) + ", 5)"}
                                     key={'river-line-green'}
                                     d=" M 5 10 L 60 10"
                                     stroke={'#51a83a'}
                                     strokeWidth={'2.25'}
                                     className={'river forward-flow'}
                                 />
-                                <text x="45" y="52.5" className="legend-label">IRRIGATION</text>
+                                <text x={(startingPoint_Paths + (increments_Paths * 0))} y="52.5" className="legend-label">IRRIGATION</text>
 
                                 <path className='flow-spark-line'
-                                    transform={"translate(195, 5)"}
+                                    transform={"translate(" + (startingPoint_Paths + (increments_Paths * 1)) + ", 5)"}
                                     key={'river-line-blue'}
                                     d=" M 5 10 L 60 10"
                                     stroke={'#428dff'}
                                     strokeWidth={'2.25'}
                                     className={'river forward-flow'}
                                 />
-                                <text x="180" y="52.5" className="legend-label">NON-IRRIGATION</text>
+                                <text x={(startingPoint_Paths + (increments_Paths * 1) - 15)} y="52.5" className="legend-label">NON-IRRIGATION</text>
 
                                 <path className='flow-spark-line'
-                                    transform={"translate(345, 5)"}
+                                    transform={"translate(" + (startingPoint_Paths + (increments_Paths * 2)) + ", 5)"}
                                     key={'river-line-gray'}
                                     d=" M 5 10 L 60 10"
                                     stroke={'#92cce3'}
                                     strokeWidth={'2.25'}
                                     className={'river forward-flow'}
                                 />
-                                <text x="354" y="52.5" className="legend-label">INFLOW</text>
+                                <text x={(startingPoint_Paths + (increments_Paths * 2) + 9)} y="52.5" className="legend-label">INFLOW</text>
 
                                 <path className='flow-spark-line'
-                                    transform={"translate(490, 5)"}
+                                    transform={"translate(" + (startingPoint_Paths + (increments_Paths * 3)) + ", 5)"}
                                     key={'river-line-orange'}
                                     d=" M 5 10 L 60 10"
                                     stroke={'#f7a02e'}
                                     strokeWidth={'2.25'}
                                     className={'river forward-flow'}
                                 />
-                                <text x="492" y="52.5" className="legend-label">DIVERSION</text>
+                                <text x={(startingPoint_Paths + (increments_Paths * 3) + 2)} y="52.5" className="legend-label">DIVERSION</text>
                             </g>
-
                         </svg>
                     </div>
                 </div>
