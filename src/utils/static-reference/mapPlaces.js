58b95729d5c4dc455f4236934acf097101df359b
RicardoDesktop <rir954@mail.usask.com>
2019-08-06 17:35:49 -0700

    Updated legend and some lngLat coordinates

--- src/utils/static-reference/mapPlaces.js
+++ src/utils/static-reference/mapPlaces.js
@@ -1,9 +1,9 @@
 export default [
     { "id": "southSask", "name": "South Saskatchewan Basin", "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/South_Saskatchewan_River.jpg/240px-South_Saskatchewan_River.jpg", "latitude": 52.56, "longitude": -105.504, "zoom": 5.5 },
     { "id": "highwood", "name": "Highwood Basin", "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Highwood_River_AB.JPG/240px-Highwood_River_AB.jpg", "latitude": 50.5, "longitude": -114.41, "zoom": 8 },
-    { "id": "reddeer", "name": "Reddeer Basin", "image": "", "latitude": 51.6, "longitude": -113, "zoom": 6.7 },
-    { "id": "northSask", "name": "North Saskatchewan Basin", "image": "", "latitude": 52.95, "longitude": -110.68, "zoom": 6 },
+    { "id": "reddeer", "name": "Reddeer Basin", "image": "", "latitude": 51.8, "longitude": -113, "zoom": 6.7 },
+    { "id": "northSask", "name": "North Saskatchewan Basin", "image": "", "latitude": 52.80, "longitude": -110.98, "zoom": 6.2 },
     { "id": "bow", "name": "Bow River", "image": "", "latitude": 50.851, "longitude": -113.883, "zoom": 7 },
     { "id": "oldman", "name": "Oldman River", "image": "", "latitude": 49.8436, "longitude": -113.376, "zoom": 7.2 },
-    { "id": "ab_southSask", "name": "AB South Saskatchewan Basin", "image": "", "latitude": 49.94, "longitude": -110.70, "zoom": 7 },
+    { "id": "ab_southSask", "name": "AB South Saskatchewan Basin", "image": "", "latitude": 50.4, "longitude": -110.70, "zoom": 6.7 },
 ]
