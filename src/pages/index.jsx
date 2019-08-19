f983f957bc6048e0cad472723112d6eb56d65249
RicardoDesktop <rir954@mail.usask.com>
2019-08-19 08:39:30 -0700

    Removed MockDashboard as it is no longer used

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 export { default as NotFound } from './NotFound';
 export { default as Home } from './Home';
 export { default as Dashboard } from './Dashboard';
-export { default as MockDashboard } from './MockDashboard';
 export { default as Parser } from './Parser';
