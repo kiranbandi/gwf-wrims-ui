d65a8ca828bb0e75a503e8fbed5c7592ae578d4c
saxena-ishan <saxena.ishan2204@gmail.com>
2019-08-25 01:15:22 -0600

    added 'close button' support for the modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -9,19 +9,31 @@ class Modal extends Component {
             modalComponents: {
                 "userSelection" : function(args) { return ( <UserSelection args={args}/> ); },
                 "infoContainer" : function(args) { return ( <InformationContainer args={args}/> ); } 
-
             }
         }
     }
     
     render() {
         
-        const { componentID = "", show = false, onClick = () => { return; }, args = []} = this.props;
+        const { componentID = "", 
+                show = false, 
+                onClick = () => { return; },
+                args = [], 
+                closeButton = false,
+                onClose = () => { return; } } = this.props;
+        
+        let crossIconStyle = { background: 'url(assets/img/cross.png)', backgroundSize: '100%' };
 
         const { modalComponents } = this.state;
 
         return (
             <div className={"modal-root" + (show? " visible" : "")} onClick={onClick}>
+                {closeButton && 
+                    <div 
+                     className="modal-cross-icon-container" 
+                     style={crossIconStyle}
+                     onClick={onClose}>
+                     </div>}
                 {modalComponents[componentID] && modalComponents[componentID](args)}    
             </div>
         );
