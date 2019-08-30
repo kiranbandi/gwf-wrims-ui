5837b8de76b0dd6e0fabf5375633acebd65f7436
Ricardo Rheeder <ricardorheeder@gmail.com>
2019-08-30 17:35:33 -0600

    Documented Artifacts.jsx

--- src/components/RiverMap/Artifacts.jsx
+++ src/components/RiverMap/Artifacts.jsx
@@ -3,12 +3,16 @@ import _ from 'lodash';
 import LegendIcon from '../MapLegend/LegendIcon'
 
 export default class Markers extends Component {
-
+    // Artifacts may either be reservoirs or sinks
     constructor(props) {
         super(props);
         this.onArtifactClick = this.onArtifactClick.bind(this);
     }
 
+    /**
+     * Serves as the onClick for markers
+     * @param {*} artifact An interactable marker on the rivermap 
+     */
     onArtifactClick(artifact) {
         this.props.onItemClick('artifact', artifact);
     }
@@ -19,7 +23,7 @@ export default class Markers extends Component {
             // scale relative to the size of the screen
             reservoirIconScale = (xScale(1) - xScale(0)) / 90;
         let tempOffset = reservoirIconScale * 150;
-
+        // Reservoirs may either be normal reservoirs or power reservoirs
         const reservoirList = _.map(_.filter(artifacts, (d) => d.type == 'reservoir'),
             (reservoir, index) => {
                 let isPowerReservoir = ["R1_LDief", "R6_Cod", "R7_Tobin"].includes(reservoir.name);
@@ -48,22 +52,20 @@ export default class Markers extends Component {
                 </g>
             })
 
-
-
         const sinkList = _.map(_.filter(artifacts, (d) => d.type == 'sink'),
             (sink, index) => {
                 const { coords, size = 1 } = sink;
                 return <g key={'sink-' + index} className={'sink' + ((highlightName == sink.name) ? ' highlight' : '')}
-                            transform={"translate(" + (+xScale(coords[0]) - (tempOffset)) + "," + (+yScale(coords[1]) - (tempOffset)) + ") scale(" + (size * reservoirIconScale) + ")"}>
-                            <circle className="sinkBorder" cx='150' cy='150' r='200'></circle>
-                            <g transform={"translate(-40, -40) scale(0.75)"}>
-                                 <path d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M256,480 C132.288,480,32,379.712,32,256S132.288,32,256,32s224,100.288,224,224S379.712,480,256,480z"/>
-                                <circle cx="256" cy="256" r="32"/>
-                                <circle cx="368" cy="256" r="32"/>
-                                <circle cx="144" cy="256" r="32"/>
-                                <title>{sink.name}</title>
-                            </g>
-                        </g>
+                    transform={"translate(" + (+xScale(coords[0]) - (tempOffset)) + "," + (+yScale(coords[1]) - (tempOffset)) + ") scale(" + (size * reservoirIconScale) + ")"}>
+                    <circle className="sinkBorder" cx='150' cy='150' r='200'></circle>
+                    <g transform={"translate(-40, -40) scale(0.75)"}>
+                        <path d="M256,0C114.615,0,0,114.615,0,256s114.615,256,256,256s256-114.615,256-256S397.385,0,256,0z M256,480 C132.288,480,32,379.712,32,256S132.288,32,256,32s224,100.288,224,224S379.712,480,256,480z" />
+                        <circle cx="256" cy="256" r="32" />
+                        <circle cx="368" cy="256" r="32" />
+                        <circle cx="144" cy="256" r="32" />
+                        <title>{sink.name}</title>
+                    </g>
+                </g>
             })
 
         return (<g className='artifacts-container'>{[...reservoirList, ...sinkList]}</g>)
